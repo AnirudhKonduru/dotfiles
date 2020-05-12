21df7ea74bef22c8ba55885c4de201d4eb458291
Anirudh Konduru <anirudhmkonduru@gmail.com>
2020-05-13 00:29:59 +0530

    add custom zsh prompt

---
+++ zsh/.zsh/prompt.zsh
@@ -0,0 +1,18 @@
+# wrapping in a function to prevent variables from leaking
+make_prompt() {
+
+    local USER=""
+    # tell me when root
+    if [[ $UID == 0 || $EUID == 0 ]];
+        then
+        USER="%F{red}root%f"
+    fi
+
+    local CWD="[%F{242}%2.%f]"
+    local END="%(?.%F{85}.%F{red})ꏸ%f"
+
+PROMPT=$USER$CWD$END
+}
+
+make_prompt;
+
