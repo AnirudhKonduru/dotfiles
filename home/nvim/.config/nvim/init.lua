3f43eb21b72f8f6113afaa367e7c3e0036dad7e6
Anirudh Konduru <anirudhmkonduru@gmail.com>
2024-02-04 23:58:37 -0800

    setup neovim
    ..finally.
    This uses Lazy for package management, the tokyo-night theme, and
    lsp-zero for LSP integration.

---
+++ home/nvim/.config/nvim/init.lua
@@ -0,0 +1,80 @@
+-- Remaps
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Leader>pv", vim.cmd.Ex)
+
+-- Options
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+-- Plugins
+
+-- Install the plugin manager: https://github.com/folke/lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- install plugins
+require("lazy").setup({
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = { style = "night" } },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "tpope/vim-fugitive" },
+  { "VonHeikemen/lsp-zero.nvim", branch = "v3.x", dependencies = {
+      {"williamboman/mason.nvim"},
+      {"williamboman/mason-lspconfig.nvim"},
+      {"neovim/nvim-lspconfig"},
+      {"hrsh7th/cmp-nvim-lsp"},
+      {"hrsh7th/nvim-cmp"},
+      {"L3MON4D3/LuaSnip"},
+    }
+  },
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+})
+
+-- Telescope remaps
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.cmd[[colorscheme tokyonight]]
+
+-- lsp setup
+local lsp_zero = require('lsp-zero')
+lsp_zero.preset("recommended")
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
