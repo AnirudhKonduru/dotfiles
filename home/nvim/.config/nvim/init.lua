b406ea8a6941aeb733d3a4b78d4168227bf4d013
Anirudh Konduru <anirudhmkonduru@gmail.com>
2024-02-25 02:20:50 -0800

    neovim: replace gitgutter with gitsigns

--- home/nvim/.config/nvim/init.lua
+++ home/nvim/.config/nvim/init.lua
@@ -46,7 +46,7 @@ require("lazy").setup({
   { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = { style = "night" } },
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "tpope/vim-fugitive" },
-  { "airblade/vim-gitgutter" },
+  { "lewis6991/gitsigns.nvim" },
   { "VonHeikemen/lsp-zero.nvim", branch = "v3.x", dependencies = {
       {"williamboman/mason.nvim"},
       {"williamboman/mason-lspconfig.nvim"},
@@ -82,6 +82,52 @@ vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix
 vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
 vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
 
+-- GitSigns setup
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Actions
+    map('n', '<leader>hs', gs.stage_hunk)
+    map('n', '<leader>hr', gs.reset_hunk)
+    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>td', gs.toggle_deleted)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
+
+
+-- theme
 vim.cmd[[colorscheme tokyonight]]
 
 -- lsp setup
