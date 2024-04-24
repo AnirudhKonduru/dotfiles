826c529dd05aac10e4f53ca7dc4adccd197b4fab
Anirudh Konduru <anirudhmkonduru@gmail.com>
2024-04-23 19:09:29 -0700

    nvim: formatting with stylua

--- home/nvim/.config/nvim/init.lua
+++ home/nvim/.config/nvim/init.lua
@@ -1,20 +1,20 @@
 -- Remaps
-vim.g.mapleader = " "
-vim.keymap.set("n", "<Leader>pv", vim.cmd.Ex)
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<Leader>pv', vim.cmd.Ex)
 
-vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("i", "kj", "<ESC>")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-j>", "j<C-e>")
-vim.keymap.set("n", "<C-k>", "k<C-y>")
+vim.keymap.set('i', 'jk', '<ESC>')
+vim.keymap.set('i', 'kj', '<ESC>')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-j>', 'j<C-e>')
+vim.keymap.set('n', '<C-k>', 'k<C-y>')
 
 -- Options
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -24,67 +24,93 @@ vim.opt.scrolloff = 5
 -- Plugins
 
 -- Install the plugin manager: https://github.com/folke/lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- install plugins
-require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
-  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = { style = "night" } },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "tpope/vim-fugitive" },
-  { "lewis6991/gitsigns.nvim" },
-  { "VonHeikemen/lsp-zero.nvim", branch = "v3.x", dependencies = {
-      {"williamboman/mason.nvim"},
-      {"williamboman/mason-lspconfig.nvim"},
-      {"neovim/nvim-lspconfig"},
-      {"hrsh7th/cmp-nvim-lsp"},
-      {"hrsh7th/nvim-cmp"},
-      {"L3MON4D3/LuaSnip"},
-    }
-  },
-  { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }},
-  { "nvimdev/lspsaga.nvim", dependencies = {
-      "nvim-treesitter/nvim-treesitter", -- optional
-      "nvim-tree/nvim-web-devicons",     -- optional
+require('lazy').setup {
+  'folke/which-key.nvim',
+  { 'folke/neoconf.nvim', cmd = 'Neoconf' },
+  'folke/neodev.nvim',
+  { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = 'night',
     },
   },
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-})
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  { 'tpope/vim-fugitive' },
+  { 'lewis6991/gitsigns.nvim' },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'neovim/nvim-lspconfig' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/nvim-cmp' },
+      { 'L3MON4D3/LuaSnip' },
+    },
+  },
+  { 'folke/trouble.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+  {
+    'nvimdev/lspsaga.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  },
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig',
+}
 
 -- Telescope remaps
-local builtin = require('telescope.builtin')
+local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fF', function() builtin.find_files({ follow = true, hidden = true }) end)
+vim.keymap.set('n', '<leader>fF', function()
+  builtin.find_files { follow = true, hidden = true }
+end)
 
 -- Trouble remaps
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+vim.keymap.set('n', '<leader>xx', function()
+  require('trouble').toggle()
+end)
+vim.keymap.set('n', '<leader>xw', function()
+  require('trouble').toggle 'workspace_diagnostics'
+end)
+vim.keymap.set('n', '<leader>xd', function()
+  require('trouble').toggle 'document_diagnostics'
+end)
+vim.keymap.set('n', '<leader>xq', function()
+  require('trouble').toggle 'quickfix'
+end)
+vim.keymap.set('n', '<leader>xl', function()
+  require('trouble').toggle 'loclist'
+end)
+vim.keymap.set('n', 'gR', function()
+  require('trouble').toggle 'lsp_references'
+end)
 
 -- GitSigns setup
-require('gitsigns').setup{
+require('gitsigns').setup {
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
 
@@ -96,107 +122,124 @@ require('gitsigns').setup{
 
     -- Navigation
     map('n', ']c', function()
-      if vim.wo.diff then return ']c' end
-      vim.schedule(function() gs.next_hunk() end)
+      if vim.wo.diff then
+        return ']c'
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
       return '<Ignore>'
-    end, {expr=true})
+    end, { expr = true })
 
     map('n', '[c', function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
+      if vim.wo.diff then
+        return '[c'
+      end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
       return '<Ignore>'
-    end, {expr=true})
+    end, { expr = true })
 
     -- Actions
     map('n', '<leader>hs', gs.stage_hunk)
     map('n', '<leader>hr', gs.reset_hunk)
-    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('v', '<leader>hs', function()
+      gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+    end)
+    map('v', '<leader>hr', function()
+      gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+    end)
     map('n', '<leader>hS', gs.stage_buffer)
     map('n', '<leader>hu', gs.undo_stage_hunk)
     map('n', '<leader>hR', gs.reset_buffer)
     map('n', '<leader>hp', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>hb', function()
+      gs.blame_line { full = true }
+    end)
     map('n', '<leader>tb', gs.toggle_current_line_blame)
     map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>hD', function()
+      gs.diffthis '~'
+    end)
     map('n', '<leader>td', gs.toggle_deleted)
 
     -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
+    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end,
 }
 
-
 -- theme
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight]]
 
 -- lsp setup
-local lsp_zero = require('lsp-zero')
-lsp_zero.preset("recommended")
+local lsp_zero = require 'lsp-zero'
+lsp_zero.preset 'recommended'
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({ buffer = bufnr })
+  lsp_zero.default_keymaps { buffer = bufnr }
 
   if vim.lsp.inlay_hint then
     vim.lsp.inlay_hint.enable(bufnr, true)
   end
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
+require('mason').setup {}
+require('mason-lspconfig').setup {
   ensure_installed = {},
   handlers = {
     lsp_zero.default_setup,
   },
-})
+}
 
-local lsp_config = require('lspconfig')
+local lsp_config = require 'lspconfig'
 
-lsp_config.rust_analyzer.setup{
+lsp_config.rust_analyzer.setup {
   settings = {
     ['rust-analyzer'] = {
       cargo = {
         allFeatures = true,
       },
       check = {
-        command = "clippy",
+        command = 'clippy',
       },
       diagnostics = {
         enable = false,
       },
-    }
-  }
+    },
+  },
 }
 
 lsp_config.lua_ls.setup {
   on_init = function(client)
+    -- I prefer stylua over lua-ls's own formatter
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
     local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
         Lua = {
           runtime = {
             -- Tell the language server which version of Lua you're using
             -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT'
+            version = 'LuaJIT',
           },
           -- Make the server aware of Neovim runtime files
           workspace = {
             checkThirdParty = false,
             library = {
-              vim.env.VIMRUNTIME
-            }
-          }
-        }
+              vim.env.VIMRUNTIME,
+            },
+          },
+        },
       })
 
-      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+      client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
     end
     return true
-  end
+  end,
 }
 
-require('lspsaga').setup({})
-
+require('lspsaga').setup {}
