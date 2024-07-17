70252c5ff1de812c2f7767e3154653bbfd2191eb
Anirudh Konduru <anirudhmkonduru@gmail.com>
2024-07-17 07:59:25 -0700

    neovim: add lsp keybinds, fix lua lsp config, etc

--- home/nvim/.config/nvim/init.lua
+++ home/nvim/.config/nvim/init.lua
@@ -40,8 +40,9 @@ vim.opt.rtp:prepend(lazypath)
 -- install plugins
 require('lazy').setup {
   'folke/which-key.nvim',
+  'tpope/vim-commentary',
   { 'folke/neoconf.nvim', cmd = 'Neoconf' },
-  'folke/neodev.nvim',
+  { 'folke/lazydev.nvim', opts = {} },
   { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
   {
     'folke/tokyonight.nvim',
@@ -73,10 +74,69 @@ require('lazy').setup {
       'nvim-treesitter/nvim-treesitter', -- optional
       'nvim-tree/nvim-web-devicons', -- optional
     },
+    config = function()
+      local lspsaga = require 'lspsaga'
+      lspsaga.setup {}
+    end,
   },
   'williamboman/mason.nvim',
   'williamboman/mason-lspconfig.nvim',
-  'neovim/nvim-lspconfig',
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc)
+            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+          -- borrowed some keybindings from kickstart.nvim
+
+          -- Jump to the definition of the word under your cursor.
+          --  This is where a variable was first declared, or where a function is defined, etc.
+          --  To jump back, press <C-t>.
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+          -- Find references for the word under your cursor.
+          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+          -- Jump to the implementation of the word under your cursor.
+          --  Useful when your language has ways of declaring types without an actual implementation.
+          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+          -- Jump to the type of the word under your cursor.
+          --  Useful when you're not sure what type a variable is and you want to see
+          --  the definition of its *type*, not where it was *defined*.
+          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+          -- Fuzzy find all the symbols in your current document.
+          --  Symbols are things like variables, functions, types, etc.
+          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+          -- Fuzzy find all the symbols in your current workspace.
+          --  Similar to document symbols, except searches over your entire project.
+          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+          -- Rename the variable under your cursor.
+          --  Most Language Servers support renaming across files, etc.
+          map('<leader>rn', '<cmd>Lspsaga rename<CR>', '[R]e[n]ame')
+
+          -- Execute a code action, usually your cursor needs to be on top of an error
+          -- or a suggestion from your LSP for this to activate.
+          map('<leader>ca', '<cmd>Lspsaga code_action<CR>', '[C]ode [A]ction')
+
+          -- Opens a popup that displays documentation about the word under your cursor
+          --  See `:help K` for why this keymap.
+          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+          -- WARN: This is not Goto Definition, this is Goto Declaration.
+          --  For example, in C this would take you to the header.
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        end,
+      })
+    end,
+  },
+  { 'github/copilot.vim', version = '1.36.0' },
 }
 
 -- Telescope remaps
@@ -182,7 +242,7 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps { buffer = bufnr }
 
   if vim.lsp.inlay_hint then
-    vim.lsp.inlay_hint.enable(bufnr, true)
+    vim.lsp.inlay_hint.enable(true, { bufnr })
   end
 end)
 
@@ -217,29 +277,10 @@ lsp_config.lua_ls.setup {
     -- I prefer stylua over lua-ls's own formatter
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentFormattingRangeProvider = false
-    local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using
-            -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-          },
-          -- Make the server aware of Neovim runtime files
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME,
-            },
-          },
-        },
-      })
-
-      client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
-    end
+    client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
     return true
   end,
+  settings = {
+    Lua = {},
+  },
 }
-
-require('lspsaga').setup {}
